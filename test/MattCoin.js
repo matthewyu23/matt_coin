b194d49581ad0cfbc406a7a0981f9a66abf345eb
matthewyu23 <matthewyu23@berkeley.edu>
2022-01-11 16:37:50 -0800

    implemented transfer

--- test/MattCoin.js
+++ test/MattCoin.js
@@ -29,4 +29,29 @@ contract('MattCoin', function(accounts) {
             assert.equal(adminBalance.toNumber(), 1000000, 'allocates initial supply to admin');
         })
     }); 
+
+    it('transfers from one account to another account', function() {
+        return MattCoin.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 999999999999); 
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'error message contains revert');
+            return tokenInstance.transfer.call(accounts[1], 25000, {from: accounts[0]});
+        }).then(function(success) {
+            assert.equal(success, true, 'returns true');
+            return tokenInstance.transfer(accounts[1], 25000, {from: accounts[0]});
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers event');
+            assert.equal(receipt.logs[0].event, 'Transfer', "should be transfer event");
+            assert.equal(receipt.logs[0].args._from, accounts[0], "should have right from");
+            assert.equal(receipt.logs[0].args._to, accounts[1], "should have right to");
+            assert.equal(receipt.logs[0].args._value, 25000, "should have right value");
+            return tokenInstance.balanceOf(accounts[1]); 
+        }).then(function(balance) {
+            assert.equal(balance, 25000, 'adds amount to recieving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance, 1000000 - 25000, 'deducts amount from sending account');
+        })
+    })
 })
