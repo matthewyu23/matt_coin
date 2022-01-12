746d0935d28c3f72c27a888ec58e1d6600bb9a16
matthewyu23 <matthewyu23@berkeley.edu>
2022-01-12 02:09:51 -0800

    implemented delegated transfer

--- contracts/MattCoin.sol
+++ contracts/MattCoin.sol
@@ -7,12 +7,21 @@ contract MattCoin {
     string public standard = 'Matt Coin v1.0'; 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-    
+    mapping(address => mapping(address => uint256)) public allowance;
+
     event Transfer(
         address indexed _from, 
         address indexed _to, 
         uint256 _value
     );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender, 
+        uint256 _value
+    );
+
+
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply;
         balanceOf[msg.sender] = _initialSupply;
@@ -24,6 +33,22 @@ contract MattCoin {
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
 
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value; 
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        emit Transfer(_from, _to, _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
         return true;
     }
 }
--- test/MattCoin.js
+++ test/MattCoin.js
@@ -54,4 +54,61 @@ contract('MattCoin', function(accounts) {
             assert.equal(balance, 1000000 - 25000, 'deducts amount from sending account');
         })
     })
+
+    it('approves tokens for delegated transfer', function() {
+        return MattCoin.deployed().then(function(instance) {
+            tokenInstance = instance; 
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success) {
+            assert.equal(success, true, 'returns true');
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0]})
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers event');
+            assert.equal(receipt.logs[0].event, 'Approval', "should be approval event");
+            assert.equal(receipt.logs[0].args._owner, accounts[0], "should have right owner");
+            assert.equal(receipt.logs[0].args._spender, accounts[1], "should have right spender");
+            assert.equal(receipt.logs[0].args._value, 100, "should have right value");
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance) {
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+        })
+    })
+
+    it('handles delegated token transfers', function() {
+        return MattCoin.deployed().then(function(instance) {
+            tokenInstance = instance; 
+            fromAccount = accounts[2]; 
+            toAccount = accounts[3]; 
+            spendingAccount = accounts[4];
+            return tokenInstance.transfer(fromAccount, 100, { from: accounts[0]});
+        }).then(function(receipt) {
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount });
+        }).then(function(receipt) {
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10000, { from: spendingAccount });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer more than balance');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount })
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer more than allowance')
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount }) 
+        }).then(function(success) {
+            assert.equal(success, true, 'transfering returns true')
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount }) 
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers event');
+            assert.equal(receipt.logs[0].event, 'Transfer', "should be transfer event");
+            assert.equal(receipt.logs[0].args._from, fromAccount, "should have right from");
+            assert.equal(receipt.logs[0].args._to, toAccount, "should have right to");
+            assert.equal(receipt.logs[0].args._value, 10, "should have right value");
+            return tokenInstance.balanceOf(fromAccount)
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 90, 'from account has correct balance')
+            return tokenInstance.balanceOf(toAccount)
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 10, 'to account has correct balance')
+            return tokenInstance.allowance(fromAccount, spendingAccount)
+        }).then(function(allowance) {
+            assert.equal(allowance, 0, 'updates allowance')
+        })
+    })
 })
