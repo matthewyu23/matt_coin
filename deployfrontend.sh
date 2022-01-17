46a9bbcc030280f542523f7ae6d933e7ef3714c0
matthewyu23 <matthewyu23@berkeley.edu>
2022-01-16 18:30:52 -0800

    compiles assets for github pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,6 +1,6 @@
 rsync -r src/ docs/
 rsync build/contracts/* docs/
 git add .
-git commit -m "comples assets for github pages"
+git commit -m "compiles assets for github pages"
 git push -u website master
 
